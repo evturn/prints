0e2407b883d856161f4bc566fe4a791fc30a4eaf
Evan Turner <ev@evturn.com>
2018-02-25 12:13:12 -0500

    Set color printer to default

--- Text/Prints.hs
+++ Text/Prints.hs
@@ -1,7 +1,11 @@
 module Text.Prints
   ( module Text.Prints.Printers.Color
   , module Text.Prints.Printers.Simple
+  , prints
   ) where
 
 import           Text.Prints.Printers.Color
 import           Text.Prints.Printers.Simple
+
+prints :: Show a => a -> IO ()
+prints x = colorPrints x
--- Text/Prints/Printers/Color.hs
+++ Text/Prints/Printers/Color.hs
@@ -1,34 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.Prints.Printers.Color
-   ( colorPrints
-   ) where
+    ( colorPrints
+    ) where
 
 import qualified Language.Haskell.HsColour           as HSC
 import qualified Language.Haskell.HsColour.Colourise as HSC
 import qualified Language.Haskell.HsColour.Output    as HSC
 import           Text.Show.Pretty                    (ppShow)
 
+colorPrints :: Show a => a -> IO ()
+colorPrints = putStrLn . format . ppShow
+
+prefs :: HSC.ColourPrefs
 prefs = HSC.defaultColourPrefs
-  { HSC.conid =
-      [ HSC.Foreground HSC.Yellow
-      , HSC.Bold
-      ]
-  , HSC.conop =
-      [ HSC.Foreground HSC.Yellow ]
-  , HSC.string =
-      [ HSC.Foreground HSC.Green ]
-  , HSC.char =
-      [ HSC.Foreground HSC.Cyan ]
-  , HSC.number =
-      [ HSC.Foreground HSC.Red
-      , HSC.Bold
-      ]
-  , HSC.layout =
-      [ HSC.Foreground HSC.White ]
-  , HSC.keyglyph =
-      [ HSC.Foreground HSC.White ]
+  { HSC.keyword          = [HSC.Foreground HSC.Yellow]
+  , HSC.comment          = [HSC.Foreground HSC.Yellow]
+  , HSC.varid            = [HSC.Foreground HSC.Yellow]
+  , HSC.varop            = [HSC.Foreground HSC.Yellow]
+  , HSC.selection        = [HSC.Foreground HSC.Yellow]
+  , HSC.variantselection = [HSC.Foreground HSC.Yellow]
+  , HSC.definition       = [HSC.Foreground HSC.Yellow]
+  , HSC.conid            = [HSC.Foreground (HSC.Rgb 95 95 255)]
+  , HSC.conop            = [HSC.Foreground HSC.Yellow]
+  , HSC.string           = [HSC.Foreground HSC.Green]
+  , HSC.char             = [HSC.Foreground HSC.Green]
+  , HSC.number           = [HSC.Foreground (HSC.Rgb 175 0 215)]
+  , HSC.layout           = [HSC.Foreground HSC.Red]
+  , HSC.keyglyph         = [HSC.Foreground HSC.Red]
   }
 
+output :: HSC.Output
+output = HSC.TTYg HSC.XTerm256Compatible
 
-colorPrints x =
-  putStrLn $ HSC.hscolour
-    (HSC.TTYg HSC.XTerm256Compatible) prefs False False "" False $ ppShow x
+format :: String -> String
+format = HSC.hscolour output prefs False False "" False
